8163c5f53f107697255f1cc62afd5cdc41d6f4e2
iamtanjil <tanjil4work@gmail.com>
2022-08-22 21:33:55 +0600

    complete error handeling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 function getInputFeildValue (inputFeildID){
     const inputFeildValue = document.getElementById(inputFeildID);
     const inputFeildString = inputFeildValue.value;
@@ -9,9 +10,33 @@ function setTextValue(ValueID, value){
     textValueByID.innerHTML = value;
 }
 
+function getTextValue (textId){
+    const textValue = document.getElementById(textId);
+    const textValueString = textValue.innerText;
+    const textValueNumber = parseFloat(textValueString);
+    return textValueNumber;
+}
+
 document.getElementById('btn-player-calc').addEventListener('click', function(){
     const playerCost = getInputFeildValue('player-cost');
     const playerCostCal = playerCost * 5;
-    setTextValue('player-cost-value',playerCostCal);
-    
+    if(playerCost === Number){
+        setTextValue('player-cost-value',playerCostCal);
+    }
+    else{
+        alert('Input a valid Nuber');
+    }
+})
+
+document.getElementById('btn-total-cal').addEventListener('click', function(){
+    const totalPlayerCost = getTextValue('player-cost-value');
+    const totalManagerCost = getInputFeildValue('manager-cost');
+    const totalCoachCost = getInputFeildValue('coach-cost');
+    const allCost = totalPlayerCost + totalManagerCost + totalCoachCost;
+    if(totalManagerCost === Number && totalCoachCost === Number){
+        setTextValue('all-cost', allCost);
+    }
+    else{
+        alert('Input a valid Nuber');
+    }
 })
